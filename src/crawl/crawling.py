0e01e9661c2fcb65d816c034894b4915b9cc38a8
kimYunHyeong <90606145+kimYunHyeong@users.noreply.github.com>
2024-12-11 04:44:34 +0900

    code clean up

--- src/crawl/crawling.py
+++ src/crawl/crawling.py
@@ -21,11 +21,11 @@ time.sleep(3)
 last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)  # 스크롤 후 대기 시간
+    time.sleep(2) 
 
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
-        break  # 스크롤 완료
+        break 
     last_height = new_height
 
 
